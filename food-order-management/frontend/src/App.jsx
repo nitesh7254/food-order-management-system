47d6ad36b37b09fb0e23f579f13da33cd7ac2f3c
Nitesh Kumar <niteshkudra311@gmail.com>
2026-09-06 15:27:54 +0530

    change component/ to Component/

--- food-order-management/frontend/src/App.jsx
+++ food-order-management/frontend/src/App.jsx
@@ -6,8 +6,8 @@ import {
   Routes,
 } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
+import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
 
 import Dashboard from "./pages/Dashboard";
 import Customers from "./pages/Customers";
